69f84f3ce8639800d596a2144cf5359185d6111b
azi <1282354268@qq.com>
2024-01-03 15:51:17 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/11111.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "11111.exe"
+  "11111.exe.manifest"
+  "11111.pdb"
+  "CMakeFiles/11111.dir/main.cpp.obj"
+  "CMakeFiles/11111.dir/main.cpp.obj.d"
+  "lib11111.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11111.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
